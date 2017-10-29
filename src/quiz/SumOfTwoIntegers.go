a56b134fdb5855fde9848c23413442e1564c1df9
nginnx <nginnx@gmail.com>
2017-10-29 23:58:49 +0800

    quiz:371

---
+++ src/quiz/SumOfTwoIntegers.go
@@ -0,0 +1,42 @@
+package leetcode
+
+import (
+	"fmt"
+)
+
+func add(a, b int) int {
+	v := a
+	v1 := b
+
+	for v1 != 0 {
+		temp := v ^ v1
+		temp1 := (v & v1) << 1
+		v = temp
+		v1 = temp1
+	}
+
+	return v
+}
+
+/*011
+011
+
+000
+0110
+
+
+110
+111
+
+
+0001
+1100
+
+1101
+0000
+
+
+
+
+
+*/
